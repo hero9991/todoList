87bd55f525b30c064ac086802989db50d42ab1af
Mger <vanyakubarin@mail.ru>
2020-11-03 23:42:36 +0300

    first correct commit

---
+++ build/precache-manifest.c668e64f5607e71d8d403abe4968b970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b32c2b0da6174e2a91c7ab3948d4b6",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "0f544a75d582aa15321a",
+    "url": "/todoList/static/css/main.bcbd86a9.chunk.css"
+  },
+  {
+    "revision": "2cc03c440bf286072e10",
+    "url": "/todoList/static/js/2.c068cca6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoList/static/js/2.c068cca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f544a75d582aa15321a",
+    "url": "/todoList/static/js/main.dd89a7e9.chunk.js"
+  },
+  {
+    "revision": "932f1ec8c1fc907ba435",
+    "url": "/todoList/static/js/runtime-main.faa2c6f2.js"
+  }
+]);
